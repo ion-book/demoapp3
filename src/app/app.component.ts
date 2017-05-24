5a0c93d424493f8c21f3a091a0204ec3183660eb
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-05-24 16:25:35 -0500

    Referencia en la plantilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user:Usuario ={
+    name: '',
+    account: {
+      email: '',
+      confirm: ''
+    }
+  };
 }
